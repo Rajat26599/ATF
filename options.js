93468fe4aa56fb7f716cb37de69386fbc9074274
Rajat <saxena1975sanjeev@gmail.com>
2023-07-07 21:16:25 +0530

    Changed storage from localStorage to chrome.storage.local

--- options.js
+++ options.js
@@ -1,85 +1,98 @@
 //import data from "./data.js";
 
 export function display(){
-    const cardsList = document.getElementsByClassName("cards")[0];
 
-    //remove all old elements
-    var child = cardsList.lastChild;
-    while(child) {
-        cardsList.removeChild(child);
-        child = cardsList?.lastChild;
-    }
-
-    //add fresh elements once again;
-    JSON.parse(localStorage.getItem("savedContent")).reverse().forEach((item, index) => {
-        const newCard = document.createElement("div");
-        newCard.setAttribute("class", "card");
+    //JSON.parse(localStorage.getItem("savedContent"))?.reverse().forEach((item, index) => {
+    chrome.storage.local.get("savedContent", (savedContent) => {
         
-        //btn panel starts
-        const btnPanel = document.createElement("div");
-        btnPanel.setAttribute("class", "btn-panel")
+        const cardsList = document.getElementsByClassName("cards")[0];
 
-        const edit = document.createElement("button");
-        edit.setAttribute("class", "edit-btn");
-        edit.setAttribute("id", item.id);
-        edit.innerHTML = "Edit";
-        btnPanel.appendChild(edit);        
-
-        const remove = document.createElement("button");
-        remove.setAttribute("class", "remove-btn");
-        remove.setAttribute("id", item.id);
-        remove.innerHTML = "x";
-        btnPanel.appendChild(remove);
-
-        newCard.appendChild(btnPanel);
-        //btn panel ends
-
-        const ifrm = document.createElement("iframe");
-        ifrm.setAttribute("src", item.link);
-        ifrm.setAttribute("class", "preview");
-        newCard.appendChild(ifrm);
-
-        const link = document.createElement("a");
-        link.innerText = item.link;
-        link.setAttribute('href', item.link);
-        link.setAttribute('target', "_blank");
-        newCard.appendChild(link);
-        
-        const tags = document.createElement("div");
-        item.tags.forEach((tagName, tagIndex) => {
-            if(tagName){
-                const tag = document.createElement("p");
-                tag.setAttribute("class", "tag");
-                tag.innerHTML = tagName;
-                tags.appendChild(tag);
-            }
-        })
-        newCard.appendChild(tags);
-
-        const createdOn = document.createElement("p");
-        createdOn.innerHTML = item.createdOn;
-        newCard.appendChild(createdOn);
-
-        cardsList.appendChild(newCard);
-    });
-
-    function remove(id){
-        if(window.confirm("Are you sure you want to delete this item?")){
-            let savedContent = JSON.parse(localStorage.getItem("savedContent"));
-            console.log(savedContent);
-            savedContent.splice(savedContent.findIndex(e => e.id === id),1);
-            console.log(savedContent);
-            localStorage.setItem("savedContent", JSON.stringify(savedContent));
-            display();
+        //remove all old elements
+        var child = cardsList.lastChild;
+        while(child) {
+            cardsList.removeChild(child);
+            child = cardsList?.lastChild;
         }
-    }
-    
-    const removeBtn = document.querySelectorAll('.remove-btn');
-    removeBtn.forEach((item) => {
-        item.addEventListener('click', (e) => {
-            console.log(e.target.id);
-            remove(e.target.id);
+        
+        //add fresh elements once again;
+        savedContent.savedContent?.reverse().forEach((item, index) => {
+            const newCard = document.createElement("div");
+            newCard.setAttribute("class", "card");
+            
+            //btn panel starts
+            const btnPanel = document.createElement("div");
+            btnPanel.setAttribute("class", "btn-panel")
+
+            const edit = document.createElement("button");
+            edit.setAttribute("class", "edit-btn");
+            edit.setAttribute("id", item.id);
+            edit.innerHTML = "Edit";
+            btnPanel.appendChild(edit);        
+
+            const remove = document.createElement("button");
+            remove.setAttribute("class", "remove-btn");
+            remove.setAttribute("id", item.id);
+            remove.innerHTML = "x";
+            btnPanel.appendChild(remove);
+
+            newCard.appendChild(btnPanel);
+            //btn panel ends
+
+            const ifrm = document.createElement("iframe");
+            ifrm.setAttribute("src", item.link);
+            ifrm.setAttribute("class", "preview");
+            newCard.appendChild(ifrm);
+
+            const link = document.createElement("a");
+            link.innerText = item.link;
+            link.setAttribute('href', item.link);
+            link.setAttribute('target', "_blank");
+            newCard.appendChild(link);
+            
+            const tags = document.createElement("div");
+            item.tags.forEach((tagName, tagIndex) => {
+                if(tagName){
+                    const tag = document.createElement("p");
+                    tag.setAttribute("class", "tag");
+                    tag.innerHTML = tagName;
+                    tags.appendChild(tag);
+                }
+            })
+            newCard.appendChild(tags);
+
+            const createdOn = document.createElement("p");
+            createdOn.innerHTML = item.createdOn?.substring(1,item.createdOn.length-1);
+            newCard.appendChild(createdOn);
+
+            cardsList.appendChild(newCard);
+        });
+
+        function remove(id){
+            if(window.confirm("Are you sure you want to delete this item?")){
+                // let savedContent = JSON.parse(localStorage.getItem("savedContent"));
+                // console.log(savedContent);
+                // savedContent.splice(savedContent.findIndex(e => e.id === id),1);
+                // console.log(savedContent);
+                // localStorage.setItem("savedContent", JSON.stringify(savedContent));
+                // display();
+                console.log(chrome.storage.local);
+                chrome.storage.local.get("savedContent", (savedContent) => {
+                    savedContent.savedContent.splice(savedContent.savedContent.findIndex(e => e.id === id),1);
+                    chrome.storage.local.set({savedContent: savedContent.savedContent}, () => {
+                        console.log("Removed Successfully!");
+                        display();
+                    });
+                })
+            }
+        }
+
+        const removeBtn = document.querySelectorAll('.remove-btn');
+        removeBtn.forEach((item) => {
+            item.addEventListener('click', (e) => {
+                remove(e.target.id);
+            });
         });
     });
+    
 }
 display();
--- popup.js
+++ popup.js
@@ -8,14 +8,35 @@ chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
 
 const btn = document.querySelector('.btn');
 
-function add(obj){
-    let savedContent = JSON.parse(localStorage.getItem("savedContent"));
-    if(savedContent!==null) localStorage.setItem("savedContent", JSON.stringify([...savedContent, obj]));
-    else localStorage.setItem("savedContent", JSON.stringify([obj]));
-    // display();
+// function add(obj){
+//     let savedContent = JSON.parse(localStorage.getItem("savedContent"));
+//     if(savedContent!==null) localStorage.setItem("savedContent", JSON.stringify([...savedContent, obj]));
+//     else localStorage.setItem("savedContent", JSON.stringify([obj]));
+//     // display();
+// }
+function addInLocal(obj){
+    chrome.storage.local.get("savedContent", (savedContent) => {
+        if(savedContent.savedContent!==null) { 
+            chrome.storage.local.set({savedContent: [...savedContent.savedContent, obj]}, () => {
+                console.log("Saved Successfully!");
+            });
+        }
+        else {
+            chrome.storage.local.set({savedContent: [obj]}, () => {
+                console.log("Saved Successfully!");
+            });
+        } 
+        console.log(savedContent);
+    });
 }
-function clear(){
-    localStorage.clear();
+
+// function clear(){
+//     localStorage.clear();
+// }
+function clearLocal(){
+    chrome.storage.local.set({savedContent: []}, () => {
+        console.log("Saved Successfully!");
+    });
 }
 
 btn.addEventListener('click', (e) => {
@@ -30,8 +51,9 @@ btn.addEventListener('click', (e) => {
         id: savedContent!=null?savedContent.length:0,
         link: popupFormData.get('url'),
         tags: tagsList,
-        createdOn: new Date(),
+        createdOn: JSON.stringify(new Date()),
     };
-    add(obj);
+    // add(obj);
+    addInLocal(obj);
     window.close();
 });
